a4e7904050f9658bae77d78bf81dc776edaa1333
extJo <josungyun6518@gmail.com>
2017-01-09 01:30:11 +0900

    Week2 Monday Study - variable
    swift에서 lLocal variable 과 global variable의  차이.

---
+++ Week2_M/variable/variable/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  variable
+//
+//  Created by 조성윤 on 2017. 1. 1..
+//  Copyright © 2017년 조성윤. All rights reserved.
+//
+
+import Foundation
+
+let ab = 30
+
+func something() {
+    let ab = 20
+    print(ab)
+    
+}
+
+something()
+// 20  --> local variable
+
+
+print(ab)
+// 30  --> global variable
